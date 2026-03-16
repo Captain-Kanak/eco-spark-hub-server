54ccf2dbceee9bb127f4fcc21ee7e605f9e47c87
Captain-Kanak <kanakroy835@gmail.com>
2026-03-16 12:21:23 +0600

    prisma setup done

---
+++ prisma/migrations/20260316061800_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "ideases" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ideases_pkey" PRIMARY KEY ("id")
+);
